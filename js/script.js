588fae80a63de4f8d807a92fd8a52ae52fc213fa
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-10-07 15:26:02 +0200

    provando ad implementare l'algoritmo

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const firstName = document.getElementById("nome");
+
+const lastName = document.getElementById("cognome");
+
+const km = parseInt (document.getElementById("km"));
+
+const eta = document.getElementById("eta");
+
+const button = document.getElementById("submit");
+
+console.log(firstName, lastName,km,eta,button, typeof(firstName, lastName,km,eta,button));
+
+const bigliettoBase = 0.21;
+console.log(bigliettoBase, typeof(bigliettoBase));
+
+const bigliettoKm = (bigliettoBase * km);
+console.log(bigliettoKm, typeof (bigliettoKm));
+
+let bigliettoScontato = bigliettoKm 
+if (eta <= 18 ) {
+    bigliettoScontato = bigliettoKm * 20 / 100; 
+} else if (eta >65) {
+    bigliettoScontato = bigliettoKm * 40 / 100;
+    }
+    console.log(bigliettoScontato, typeof(bigliettoScontato));
